14bd700a778c3c463f55fcb362378ac6353b49b5
kiityz <caroline.demaubeuge@gmail.com>
2015-06-09 21:53:16 +0200

    landing page FR/EN

---
+++ includes/en.inc.php
@@ -0,0 +1,25 @@
+<?
+$trad = array();
+$lang = "en";
+$trad["h1"]="Share your services";
+$trad["description"]="Shiip is the mobile application who allows you to share your services with other people
+it’s easy and free"; 
+$trad["button"]="Download the app!";
+$trad["plat"]="An ergonomic platform";
+$trad["look"]="Look for. Select. ";
+$trad["look2"]="Contact. ";
+$trad["look3"]="Look for the service you need and select someone who can help you. You can easily visit his profil to see if he was recommend by the other";
+$trad["hash"]="Hashtags";
+$trad["hash2"]="Categorize wat <br/>you are looking for.";
+$trad["hash3"]="The search bar is using hashtags to look for the service you need.";
+$trad["com"]="A community who is close";
+$trad["com2"]="An unique experience<br/> of sharing.";
+$trad["com3"]="Find users who are not far from you and go look for them";
+$trad["test"]="I study nursery. Shiip allowed me to find someone to help me in my english courses. A great experience !";
+$trad["test2"]="An incredible experience, a lot of could things wit this application !";
+$trad["test3"]="An saturday in the afternoon, I was able to help an old lady. She made the best 'Vol au vent' I had ever eat !";
+$trad["tente"]="Try the experience !";
+$trad["dl"]="Download";
+$trad["cond"]="Terms of use";
+$trad["priv"]="Privacy policies";
+$trad["copy"]="copyright 2015, Shiip by Caroline De Maubeuge";
